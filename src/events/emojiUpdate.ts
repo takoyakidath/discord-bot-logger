4b20c09c0371f0985ba037e8f5c72e9072408d28
minagishl <mvxd568.co@icloud.com>
2025-04-08 13:55:13 +0900

    feat: add update notification for emojis

---
+++ src/events/emojiUpdate.ts
@@ -0,0 +1,39 @@
+import { Events, GuildEmoji, EmbedBuilder, TextChannel } from "discord.js";
+import logger from "~/utils/logger";
+
+export default {
+  name: Events.GuildEmojiUpdate,
+  async execute(oldEmoji: GuildEmoji, newEmoji: GuildEmoji): Promise<void> {
+    logger.info(
+      newEmoji.id,
+      `Emoji update started: ${oldEmoji.name} -> ${newEmoji.name}`
+    );
+    const channelId = process.env.CHANNEL_ID;
+    if (!channelId) return;
+    const logChannel = newEmoji.client.channels.cache.get(
+      channelId
+    ) as TextChannel;
+    if (!logChannel) return;
+
+    const details = [
+      "**Old Details:**",
+      `・Name: **${oldEmoji.name}**`,
+      `・Animated: **${oldEmoji.animated ? "Yes" : "No"}**`,
+      `・ID: **${oldEmoji.id}**`,
+      "\n**New Details:**",
+      `・Name: **${newEmoji.name}**`,
+      `・Animated: **${newEmoji.animated ? "Yes" : "No"}**`,
+      `・ID: **${newEmoji.id}**`,
+    ].join("\n");
+
+    const embed = new EmbedBuilder()
+      .setTitle("Emoji Updated")
+      .setColor(0xffaa00)
+      .setDescription(details)
+      .setThumbnail(newEmoji.url)
+      .setTimestamp();
+
+    await logChannel.send({ embeds: [embed] });
+    logger.info(newEmoji.id, "Emoji update logged.");
+  },
+};
